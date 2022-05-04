1aa4867087a3b476167842a887a06df4a19c56bd
nanarama <annasampers@gmail.com>
2022-05-04 16:12:27 -0500

    Wrote functioning computerPlay

--- script.js
+++ script.js
@@ -4,15 +4,15 @@ const computerSelection = computerPlay();
 //variable to store player's selection
 const playerSelection = 'rock';
 
-//variables to store selection options
-const computerOption = ['Rock', 'Paper', 'Scissors'];
-
-
 //function that randomly generates the computer's selection
-function computerPlay() {
-
+function computerPlay(option) {
+    const options = ['Rock', 'Paper', 'Scissors'];
+    option = options[Math.floor(Math.random() * options.length)];
+    return option;
 }
 
+
+console.log(computerSelection);
 //function that plays a single round of RPS, using playerSelection
 //and computerSelection, returning a string that declares the winner
 //function that generates 5 playRounds, keeping score, and reports 
