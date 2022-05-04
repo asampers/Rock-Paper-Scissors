9bc9c8cf5f1d957f7cf44b8457bb5503d0804272
nanarama <annasampers@gmail.com>
2022-05-04 15:12:01 -0500

    Define initial variables

---
+++ script.js
@@ -0,0 +1,19 @@
+//variable to store computer's selection
+const computerSelection = computerPlay();
+
+//variable to store player's selection
+const playerSelection = 'rock';
+
+//variables to store selection options
+const computerOption = ['Rock', 'Paper', 'Scissors'];
+
+
+//function that randomly generates the computer's selection
+function computerPlay() {
+
+}
+
+//function that plays a single round of RPS, using playerSelection
+//and computerSelection, returning a string that declares the winner
+//function that generates 5 playRounds, keeping score, and reports 
+//a winner/loser at the end
