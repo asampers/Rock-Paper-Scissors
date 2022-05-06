1e0daaebf3354e7b8e711474494e590bd44e2917
nanarama <annasampers@gmail.com>
2022-05-06 12:42:25 -0500

    Who knows what's happening here

--- script.js
+++ script.js
@@ -1,12 +1,6 @@
-//variable to store computer's selection
-let computerSelection = computerPlay();
-
 //variable to store player's selection
 let playerSelection = prompt('Type Rock, Paper, or Scissors.').toLowerCase();
 
-//variable to store You Lose, You Win, It's a Tie(?)
-let youLose = 'You Lose! ' + (computerSelection) + ' beats ' + (playerSelection) + '.';
-let youWin = 'You Win! ' + (playerSelection) + ' beats ' + (computerSelection) + '.';
 
 //function or loop to check that player input is actually R, P, or S
 
@@ -17,21 +11,13 @@ function computerPlay(option) {
     return option;
 }
 
-//function that generates 5 playRounds, keeping score, and reports 
-//a winner/loser at the end 
-function game() {
-    for (let i = 0; i < 5; i++) {
-        if (i < 5) {
-            console.log('Starting game number ' + (i) + '') ;
-            playRound(playerSelection, computerPlay);
-            console.log(playRound(playerSelection, computerSelection)); 
-            playerSelection = prompt('Type Rock, Paper, or Scissors.').toLowerCase();
-            console.log(playerSelection);
-            console.log(computerSelection);
-            youLose = 'You Lose! ' + (computerSelection) + ' beats ' + (playerSelection) + '.';
-            youWin = 'You Win! ' + (playerSelection) + ' beats ' + (computerSelection) + '.';  
-        } else console.log('You\'ve played 5 times.');      
-}}
+//variable to store computer's selection
+let computerSelection = computerPlay();
+
+//variable to store You Lose, You Win, It's a Tie(?)
+let youLose = 'You Lose! ' + (computerSelection) + ' beats ' + (playerSelection) + '.';
+let youWin = 'You Win! ' + (playerSelection) + ' beats ' + (computerSelection) + '.';
+
 //function that plays a single round of RPS, using playerSelection
 //and computerSelection, returning a string that declares the winner
 function playRound(playerSelection, computerSelection) {
@@ -50,8 +36,26 @@ function playRound(playerSelection, computerSelection) {
      } else if (playerSelection == computerSelection) {
         return 'It\'s a tie.';
     } 
-     
-}
+    youLose = 'You Lose! ' + (computerSelection) + ' beats ' + (playerSelection) + '.';
+    youWin = 'You Win! ' + (playerSelection) + ' beats ' + (computerSelection) + '.';
+} 
+  
+
+//function that generates 5 playRounds, keeping score, and reports 
+//a winner/loser at the end 
+function game() {
+    for (let i = 0; i < 5; i++) {
+        if (i < 5) {
+            console.log('Starting game number ' + (i) + '') ;
+            console.log(playRound(playerSelection, computerPlay())); 
+            youLose = 'You Lose! ' + (computerSelection) + ' beats ' + (playerSelection) + '.';
+            youWin = 'You Win! ' + (playerSelection) + ' beats ' + (computerSelection) + '.';
+            playerSelection = prompt('Type Rock, Paper, or Scissors.').toLowerCase(); 
+            console.log(computerSelection);
+            console.log(playerSelection); 
+        } else console.log('You\'ve played 5 times.');      
+}}
+
 
 console.log(computerSelection);
 console.log(playerSelection);
